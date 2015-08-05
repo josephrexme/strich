173eaf1148f778ca2ab47d8089bb8101f1776b59
bl4ckdu5t <joerexko@gmail.com>
2015-08-05 05:39:30 -0500

    Setting up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
+var postcss = require('gulp-postcss');
 var gulp = require('gulp');
+var autoprefixer = require('autoprefixer')
+var mqpacker = require('css-mqpacker');
+var csswring = require('csswring');
 
 gulp.task('default', function(){
   // default tasks
